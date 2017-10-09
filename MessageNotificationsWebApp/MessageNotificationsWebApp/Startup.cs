7a502bb8f922735178f43ee83d1747a6082bdade
Luke Billington <luke.billington@esendex.com>
2017-10-09 16:43:51 +0100

    Initial commit, new sln

---
+++ MessageNotificationsWebApp/MessageNotificationsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageNotificationsWebApp.Startup))]
+namespace MessageNotificationsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
